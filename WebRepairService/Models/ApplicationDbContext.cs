e8274474c90e8d64f77bb3e4bbef507b34041578
argecyplusplus <tema.chernyuk@gmail.com>
2025-04-14 22:28:54 +0300

    Update ApplicationDbContext.cs

--- WebRepairService/Models/ApplicationDbContext.cs
+++ WebRepairService/Models/ApplicationDbContext.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Reflection.Emit;
 
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
@@ -18,29 +16,53 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         base.OnModelCreating(builder);
 
-        builder.Entity<Order>()
-            .HasOne(o => o.User)
-            .WithMany(u => u.Orders)
-            .HasForeignKey(o => o.UserId);
+        // Order relationships
+        builder.Entity<Order>(entity =>
+        {
+            entity.HasIndex(o => o.StatusId);
+            entity.HasIndex(o => o.CreationDate);
+            entity.HasIndex(o => o.UserId);
 
-        builder.Entity<Order>()
-            .HasOne(o => o.Status)
-            .WithMany(s => s.Orders)
-            .HasForeignKey(o => o.StatusId);
+            entity.Property(o => o.Model).HasMaxLength(100);
+            entity.Property(o => o.Details).HasMaxLength(1000);
+            entity.Property(o => o.ClientEmail).HasMaxLength(100);
 
-        builder.Entity<Order>()
-            .HasOne(o => o.DeviceType)
-            .WithMany(d => d.Orders)
-            .HasForeignKey(o => o.DeviceTypeId);
+            entity.HasOne(o => o.User)
+                .WithMany(u => u.Orders)
+                .HasForeignKey(o => o.UserId);
 
-        builder.Entity<Order>()
-            .HasOne(o => o.ServiceType)
-            .WithMany(s => s.Orders)
-            .HasForeignKey(o => o.ServiceTypeId);
+            entity.HasOne(o => o.Status)
+                .WithMany(s => s.Orders)
+                .HasForeignKey(o => o.StatusId);
 
-        builder.Entity<Photo>()
-            .HasOne(p => p.Order)
-            .WithMany(o => o.Photos)
-            .HasForeignKey(p => p.OrderId);
+            entity.HasOne(o => o.DeviceType)
+                .WithMany(d => d.Orders)
+                .HasForeignKey(o => o.DeviceTypeId);
+
+            entity.HasOne(o => o.ServiceType)
+                .WithMany(s => s.Orders)
+                .HasForeignKey(o => o.ServiceTypeId);
+        });
+
+        // Photo configuration
+        builder.Entity<Photo>(entity =>
+        {
+            entity.HasIndex(p => p.OrderId);
+
+            entity.Property(p => p.Link)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            entity.HasOne(p => p.Order)
+                .WithMany(o => o.Photos)
+                .HasForeignKey(p => p.OrderId)
+                .OnDelete(DeleteBehavior.Cascade); // Или Restrict в зависимости от требований
+        });
+
+        // Дополнительные настройки для других сущностей
+        builder.Entity<Status>()
+            .Property(s => s.Name)
+            .IsRequired()
+            .HasMaxLength(50);
     }
 }
