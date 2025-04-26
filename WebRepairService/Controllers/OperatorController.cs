95ca7de7a262df6e9e796abb311cc6ebaf3a1c2c
argecyplusplus <tema.chernyuk@gmail.com>
2025-04-26 19:47:32 +0300

    панели ролей

--- WebRepairService/Controllers/OperatorController.cs
+++ WebRepairService/Controllers/OperatorController.cs
@@ -12,7 +12,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace WebRepairService.Controllers
 {
-    [Authorize(Roles = "Operator")]
+    [Authorize(Roles = "Operator,Admin")]
     public class OperatorController : Controller
     {
         private readonly ApplicationDbContext _context;
